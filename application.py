ca093f67db204c0b73d8180142ee40604a16996a
anonyman637 <anonyman637@gmail.com>
2020-05-03 18:31:13 -0400

    Setup to respond to signup

--- application.py
+++ application.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
     print("Handling Request to Home Page")
     return "Hello, World! Azure 637"
 
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    result = {"fulfillmentText": request.get_json(force=True)}
+    return make_response(jsonify(result))
+
+if __name__ == "__main__":
+    app.run(debug=True)
