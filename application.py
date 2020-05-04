a0e3f63aaaaddab3f36493aaa162071ac8f6f6db
anonyman637 <anonyman637@gmail.com>
2020-05-03 21:27:16 -0400

    First Successful working code to get user profile

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, make_response, jsonify
+import facebook
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,12 +9,19 @@ def hello_world():
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    req = request.get_json(force=True)
+    user_id = req.get("originalDetectIntentRequest").get("payload").get("data").get("sender").get("id")
+    # TODO: Refactor the following code into load profile
+    page_access_token = "EAADwzWGxszoBANpu8MZCZANCOWFxQtw4ZBsIsG2xPsba56OinacxXUBkLYrrKl6OZAXKGa8CKK6AJ5Y11ZAJESn4haADCh0r4712xjeHsT2LIcFhwMZCOgsAGZCByKLsOwZAQcd1kV47tLrvT0aI3sOJiUj5ZBKFTIAi4KLDUmnzhvrjIkeimZCaBk"
+    graph = facebook.GraphAPI(page_access_token)
+    # fields = ["first_name", "last_name", "gender"]
+    profile = graph.get_object(user_id)
     result = {
             "fulfillmentText": str(request.get_json(force=True)),
             "fulfillmentMessages": [
                 {
                     "text": {
-                        "text": ["Text response from webhook"]
+                        "text": ["Hello " + profile["first_name"] + ".\nHooray!!! Fb, Ggl, MS, and Python works together!!!"]
                         }
                     }
                 ]
