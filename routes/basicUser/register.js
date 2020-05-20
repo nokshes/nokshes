635ffce86760bc7a9d09b479ae895d0bdf0fbef9
anonyman637 <anonyman637@gmail.com>
2020-05-20 14:38:41 -0400

    Fixed Bug in registration logic

--- routes/basicUser/register.js
+++ routes/basicUser/register.js
@@ -16,36 +16,35 @@ const register = async (_psId, _unId, _isAdmin) => {
   // TODO (May 09, 2020): Validate the correctness of universityId
   let message;
   try {
-    const registeredUser = await getUserByUnId(_unId);
-    if (registeredUser) {
-      if (registeredUser.psId == _psId) {
-        message = "You're already registered.";
-      } else {
-        message = `There is already a registered user with University ID: ${_unId}`;
-      }
-      return message;
-    }
-
     const user = await getUserByPsId(_psId);
-    if (user.regStatus == 2) {
+    if (user.registeredUser == 1) {
+      message = "You're already registered.";
+      return message;
+    } else if (user.regStatus == 2) {
       message = "Your previous registration application is already PENDING.";
       return message;
     }
 
+    const registeredUser = await getUserByUnId(_unId);
+    if (registeredUser) {
+      message = `There is already a registered user with University ID: ${_unId}`;
+      return message;
+    }
+
     // find the Admin who is capable of approving his ID, i.e CR or more higher authority
-    let adminPsIds = await getClassAdminPsIds(_unId);
-    if (_isAdmin && adminPsIds.length > 1) {
+    let admins = await getClassAdminPsIds(_unId);
+    if (_isAdmin && admins.length > 1) {
       message =
         "There are already sufficient number of registered admins for your class." +
         "Your registration will be processed as a regular student.";
-    } else if (!_isAdmin && adminPsIds.length >= 1) {
+    } else if (!_isAdmin && admins.length >= 1) {
       message = "Your registration will be processed as a regular student.";
-    } else if (!_isAdmin && adminPsIds.length < 1) {
+    } else if (!_isAdmin && admins.length < 1) {
       message =
         "Your class doesn't have enough registered admins to review your application.";
       return message;
     } else {
-      adminPsIds = await getSeniorAdminPsIds(_unId);
+      admins = await getSeniorAdminPsIds(_unId);
       message = "Your registration will be processed as an administrator.";
     }
 
@@ -64,7 +63,7 @@ const register = async (_psId, _unId, _isAdmin) => {
 
     await sendReqPostCard(
       profile,
-      adminPsIds[Math.floor(Math.random() * adminPsIds.length)].psId
+      admins[Math.floor(Math.random() * admins.length)].psId
     );
     message +=
       "Thank you for being patient. Your registration has been processed.";
